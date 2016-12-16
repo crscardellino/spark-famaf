3c8936028c6fe1ed23036908d5dd37a92fce9d9d
Cristian Cardellino <crscardellino@gmail.com>
2016-12-16 15:51:01 -0300

    Reformatted the code, make it more readable

--- final/HCDN/src/main/scala/postprocessor/HCDNTopicAnalysis.scala
+++ final/HCDN/src/main/scala/postprocessor/HCDNTopicAnalysis.scala
@@ -1,7 +1,7 @@
 package postprocessor
 
 import java.io.File
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import scala.collection.JavaConversions._
 
@@ -64,6 +64,59 @@ object HCDNTopicAnalysis {
     }
   }
 
+  private def saveTopTopics(transformationsDataFrame: DataFrame, LDATopics: DataFrame,
+                           spark: SparkSession, savePath: String): Unit = {
+    import spark.implicits._
+
+    val topTopics: DataFrame = transformationsDataFrame.orderBy($"id", desc("topicDistribution"))
+      .groupBy($"id")
+        .agg(first($"pos").as("maxTopic"), max($"topicDistribution"))
+      .groupBy($"maxTopic")
+        .count
+        .orderBy(desc("count"))
+        .limit(5)
+
+    LDATopics.join(topTopics, LDATopics("topic") === topTopics("maxTopic"))
+      .map(row => (row.getAs[Seq[String]]("terms").mkString(" "), row.getAs[Long]("count")))
+      .coalesce(1).write.format("csv").save(savePath)
+  }
+
+  private def saveTopTopicsForTopColumn(dataset: DataFrame, groupColumn: Column,
+                                        LDATopics: DataFrame, LDATransformations: DataFrame,
+                                        spark: SparkSession, minimumDistribution: Double,
+                                        savePath: String): Unit = {
+    import spark.implicits._
+
+    val topInColumn: Set[String] = dataset
+      .groupBy(groupColumn)
+        .agg(countDistinct($"id").as("count"))
+        .orderBy(desc("count"))
+        .limit(10)
+      .select(groupColumn)
+        .map(row => row.getString(0)).collectAsList.toSet
+
+    val topTopicsForTopParties: DataFrame = LDATransformations.join(dataset, "id")
+      .select($"id", groupColumn, $"pos".as("topic"), $"topicDistribution")
+        .filter(row => topInColumn contains row.getAs[String](groupColumn.toString))
+        .where($"topicDistribution" >= minimumDistribution)
+        .dropDuplicates("id", "topic")
+      .groupBy(groupColumn, $"topic")
+        .agg(avg($"topicDistribution").as("topicDistributionAvg"))
+        .orderBy(groupColumn, desc("topicDistributionAvg"))
+      .groupBy(groupColumn)
+        .agg(first($"topic").as("maxTopic"), max($"topicDistributionAvg").as("topicDistributionAvgMax"))
+        .orderBy(groupColumn)
+
+    LDATopics.join(topTopicsForTopParties, LDATopics("topic") === topTopicsForTopParties("maxTopic"))
+      .map(row => (
+        row.getAs[String](groupColumn.toString),
+        row.getAs[Int]("topic"),
+        row.getAs[Double]("topicDistributionAvgMax"),
+        row.getAs[Seq[String]]("terms").mkString(" ")
+      ))
+      .coalesce(1).write.format("csv").save(savePath)
+  }
+
   def run(params: Params): Unit = {
     val inputDatasetDir: String = new File(params.inputDatasets).getAbsolutePath
     val inputTopicsPath: String = new File(params.inputLDATopics).getAbsolutePath
@@ -78,7 +131,6 @@ object HCDNTopicAnalysis {
 
     import spark.implicits._
 
-    val laws = spark.read.parquet(s"$inputDatasetDir/laws.parquet")
     val results = spark.read.parquet(s"$inputDatasetDir/results.parquet")
     val signers = spark.read.parquet(s"$inputDatasetDir/signers.parquet")
     val deputyCommissions = spark.read.parquet(s"$inputDatasetDir/deputyCommissions.parquet")
@@ -88,159 +140,42 @@ object HCDNTopicAnalysis {
     val LDATransformations: DataFrame = spark.read.parquet(s"$inputTransformedPath")
 
     // Topic with the most projects
-    val topTopics: DataFrame = LDATransformations.orderBy($"id", desc("topicDistribution"))
-      .groupBy($"id").agg(first($"pos").as("maxTopic"), max($"topicDistribution"))
-      .groupBy($"maxTopic").count.orderBy(desc("count")).limit(5)
-
-    LDATopics.join(topTopics, LDATopics("topic") === topTopics("maxTopic"))
-      .map(row => (row.getAs[Seq[String]]("terms").mkString(" "), row.getAs[Long]("count")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topTopics.csv")
+    saveTopTopics(LDATransformations, LDATopics, spark, s"$outputDirPath/topTopics.csv")
 
     // Topic with the most sanctioned projects
     val sanctionedProjects: DataFrame = results.filter($"results" === "SANCIONADO").select($"id")
-
-    val topSanctionedTopics: DataFrame = LDATransformations.join(sanctionedProjects, "id")
-      .orderBy($"id", desc("topicDistribution")).groupBy($"id")
-      .agg(max($"topicDistribution"), first($"pos").as("maxTopic"))
-      .groupBy($"maxTopic").count().orderBy(desc("count")).limit(5)
-
-    LDATopics.join(topSanctionedTopics, LDATopics("topic") === topSanctionedTopics("maxTopic"))
-      .map(row => (row.getAs[Seq[String]]("terms").mkString(" "), row.getAs[Long]("count")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topSanctionedTopics.csv")
+    saveTopTopics(LDATransformations.join(sanctionedProjects, "id"),
+      LDATopics, spark, s"$outputDirPath/topSanctionedTopics.csv")
 
     // Topic with the most projects with half sanctions
     val halfSanctionedProjects: DataFrame = results.filter($"results" === "MEDIA SANCION").select($"id")
-
-    val topHalfSanctionedTopics: DataFrame = LDATransformations.join(halfSanctionedProjects, "id")
-      .orderBy($"id", desc("topicDistribution")) .groupBy($"id")
-      .agg(max($"topicDistribution"), first($"pos").as("maxTopic"))
-      .groupBy($"maxTopic").count().orderBy(desc("count")).limit(5)
-
-    LDATopics.join(topHalfSanctionedTopics, LDATopics("topic") === topHalfSanctionedTopics("maxTopic"))
-      .map(row => (row.getAs[Seq[String]]("terms").mkString(" "), row.getAs[Long]("count")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topHalfSanctionedTopics.csv")
+    saveTopTopics(LDATransformations.join(halfSanctionedProjects, "id"),
+      LDATopics, spark, s"$outputDirPath/topHalfSanctionedTopics.csv")
 
     // Topic with the most retired projects
     val retiredProjects: DataFrame = results.filter($"results" === "RETIRADO").select($"id")
-
-    val topRetiredTopics: DataFrame = LDATransformations.join(retiredProjects, "id")
-      .orderBy($"id", desc("topicDistribution")) .groupBy($"id")
-      .agg(max($"topicDistribution"), first($"pos").as("maxTopic"))
-      .groupBy($"maxTopic").count().orderBy(desc("count")).limit(5)
-
-    LDATopics.join(topRetiredTopics, LDATopics("topic") === topRetiredTopics("maxTopic"))
-      .map(row => (row.getAs[Seq[String]]("terms").mkString(" "), row.getAs[Long]("count")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topRetiredTopics.csv")
+    saveTopTopics(LDATransformations.join(retiredProjects, "id"),
+      LDATopics, spark, s"$outputDirPath/topRetiredTopics.csv")
 
     // Top topic in the top 10 parties
-    val topParties: Set[String] = signers.groupBy($"party")
-      .agg(countDistinct($"id").as("count"))
-      .orderBy(desc("count")).limit(10).select($"party")
-      .map(row => row.getString(0)).collectAsList.toSet
-
-    val topTopicsForTopParties: DataFrame = LDATransformations.join(signers, "id")
-      .select($"id", $"party", $"pos".as("topic"), $"topicDistribution")
-      .filter(row => topParties contains row.getAs[String]("party")).dropDuplicates("id", "topic")
-      .where($"topicDistribution" >= params.minimumDistribution)
-      .agg(avg($"topicDistribution").as("topicDistributionAvg"))
-      .orderBy($"party", desc("topicDistributionAvg")).groupBy($"party")
-      .agg(first($"topic").as("maxTopic"), max($"topicDistributionAvg").as("topicDistributionAvgMax"))
-      .orderBy($"party")
-
-    LDATopics.join(topTopicsForTopParties, LDATopics("topic") === topTopicsForTopParties("maxTopic"))
-      .map(row =>
-        (row.getAs[String]("party"), row.getAs[Int]("topic"), row.getAs[Double]("topicDistributionAvgMax"),
-         row.getAs[Seq[String]]("terms").mkString(" ")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topTopicsForTopParties.csv")
-
+    saveTopTopicsForTopColumn(signers, $"party", LDATopics, LDATransformations, spark, params.minimumDistribution,
+      s"$outputDirPath/topTopicsForTopParties.csv")
 
     // Top topic in the top 10 districts
-    val topDistricts: Set[String] = signers.groupBy($"district")
-      .agg(countDistinct($"id").as("count"))
-      .orderBy(desc("count")).limit(10).select($"district")
-      .map(row => row.getString(0)).collectAsList.toSet
-
-    val topTopicsForTopDistricts: DataFrame = LDATransformations.join(signers, "id")
-      .select($"id", $"district", $"pos".as("topic"), $"topicDistribution")
-      .filter(row => topDistricts contains row.getAs[String]("district")).dropDuplicates("id", "topic")
-      .where($"topicDistribution" >= params.minimumDistribution)
-      .agg(avg($"topicDistribution").as("topicDistributionAvg"))
-      .orderBy($"district", desc("topicDistributionAvg")).groupBy($"district")
-      .agg(first($"topic").as("maxTopic"), max($"topicDistributionAvg").as("topicDistributionAvgMax"))
-      .orderBy($"district")
-
-    LDATopics.join(topTopicsForTopDistricts, LDATopics("topic") === topTopicsForTopDistricts("maxTopic"))
-      .map(row =>
-        (row.getAs[String]("district"), row.getAs[Int]("topic"), row.getAs[Double]("topicDistributionAvgMax"),
-          row.getAs[Seq[String]]("terms").mkString(" ")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topTopicsForTopDistricts.csv")
+    saveTopTopicsForTopColumn(signers, $"district", LDATopics, LDATransformations, spark, params.minimumDistribution,
+      s"$outputDirPath/topTopicsForTopDistricts.csv")
 
     // Top topic in the top 10 congressmen
-    val topCongressmen: Set[String] = signers.groupBy($"congressman")
-      .agg(countDistinct($"id").as("count"))
-      .orderBy(desc("count")).limit(10).select($"congressman")
-      .map(row => row.getString(0)).collectAsList.toSet
-
-    val topTopicsForTopCongressmen: DataFrame = LDATransformations.join(signers, "id")
-      .select($"id", $"congressman", $"pos".as("topic"), $"topicDistribution")
-      .filter(row => topCongressmen contains row.getAs[String]("congressman")).dropDuplicates("id", "topic")
-      .where($"topicDistribution" >= params.minimumDistribution)
-      .agg(avg($"topicDistribution").as("topicDistributionAvg"))
-      .orderBy($"congressman", desc("topicDistributionAvg")).groupBy($"congressman")
-      .agg(first($"topic").as("maxTopic"), max($"topicDistributionAvg").as("topicDistributionAvgMax"))
-      .orderBy($"congressman")
-
-    LDATopics.join(topTopicsForTopCongressmen, LDATopics("topic") === topTopicsForTopCongressmen("maxTopic"))
-      .map(row =>
-        (row.getAs[String]("congressman"), row.getAs[Int]("topic"), row.getAs[Double]("topicDistributionAvgMax"),
-          row.getAs[Seq[String]]("terms").mkString(" ")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topTopicsForTopCongressmen.csv")
+    saveTopTopicsForTopColumn(signers, $"congressman", LDATopics, LDATransformations, spark, params.minimumDistribution,
+      s"$outputDirPath/topTopicsForTopCongressmen.csv")
 
     // Top topic in the top 10 deputy commissions
-
-    val topDeputyCommissions: Set[String] = deputyCommissions.groupBy($"commission")
-      .agg(countDistinct($"id").as("count"))
-      .orderBy(desc("count")).limit(10).select($"commission")
-      .map(row => row.getString(0)).collectAsList.toSet
-
-    val topTopicsForTopDeputyCommissions: DataFrame = LDATransformations.join(deputyCommissions, "id")
-      .select($"id", $"commission", $"pos".as("topic"), $"topicDistribution")
-      .filter(row => topDeputyCommissions contains row.getAs[String]("commission")).dropDuplicates("id", "topic")
-      .where($"topicDistribution" >= params.minimumDistribution)
-      .agg(avg($"topicDistribution").as("topicDistributionAvg"))
-      .orderBy($"commission", desc("topicDistributionAvg")).groupBy($"commission")
-      .agg(first($"topic").as("maxTopic"), max($"topicDistributionAvg").as("topicDistributionAvgMax"))
-      .orderBy($"commission")
-
-    LDATopics.join(
-      topTopicsForTopDeputyCommissions, LDATopics("topic") === topTopicsForTopDeputyCommissions("maxTopic")
-    ).map(row =>
-        (row.getAs[String]("commission"), row.getAs[Int]("topic"), row.getAs[Double]("topicDistributionAvgMax"),
-          row.getAs[Seq[String]]("terms").mkString(" ")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topTopicsForTopDeputyCommission.csv")
+    saveTopTopicsForTopColumn(deputyCommissions, $"commission", LDATopics, LDATransformations, spark,
+      params.minimumDistribution, s"$outputDirPath/topTopicsForTopDeputyCommissions.csv")
 
     // Top topic in the top 10 senate commissions
-
-    val topSenateCommissions: Set[String] = senateCommissions.groupBy($"commission")
-      .agg(countDistinct($"id").as("count"))
-      .orderBy(desc("count")).limit(10).select($"commission")
-      .map(row => row.getString(0)).collectAsList.toSet
-
-    val topTopicsForTopSenateCommissions: DataFrame = LDATransformations.join(senateCommissions, "id")
-      .select($"id", $"commission", $"pos".as("topic"), $"topicDistribution")
-      .filter(row => topSenateCommissions contains row.getAs[String]("commission")).dropDuplicates("id", "topic")
-      .where($"topicDistribution" >= params.minimumDistribution)
-      .agg(avg($"topicDistribution").as("topicDistributionAvg"))
-      .orderBy($"commission", desc("topicDistributionAvg")).groupBy($"commission")
-      .agg(first($"topic").as("maxTopic"), max($"topicDistributionAvg").as("topicDistributionAvgMax"))
-      .orderBy($"commission")
-
-    LDATopics.join(
-      topTopicsForTopSenateCommissions, LDATopics("topic") === topTopicsForTopSenateCommissions("maxTopic")
-    ).map(row =>
-      (row.getAs[String]("commission"), row.getAs[Int]("topic"), row.getAs[Double]("topicDistributionAvgMax"),
-        row.getAs[Seq[String]]("terms").mkString(" ")))
-      .coalesce(1).write.format("csv").save(s"$outputDirPath/topTopicsForTopSenateCommission.csv")
+    saveTopTopicsForTopColumn(senateCommissions, $"commission", LDATopics, LDATransformations, spark,
+      params.minimumDistribution, s"$outputDirPath/topTopicsForTopSenateCommissions.csv")
   }
 }
 
